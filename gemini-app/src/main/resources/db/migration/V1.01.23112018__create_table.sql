b95cbda8c1784d58dfb73857cc760bc61b2f65f0
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-23 21:50:24 +0700

    #6 - Add flyway migration script Database - gemini-app module

--- gemini-app/src/main/resources/db/migration/V1.01.23112018__create_table.sql
+++ gemini-app/src/main/resources/db/migration/V1.01.23112018__create_table.sql
@@ -1,22 +1,7 @@
--
-Check
-existence
-so
-that
-tables
-do
-not
-overlap,
-delete
-if
-there
-are
+-- Check existence so that tables do not overlap, delete if there are
 DROP TABLE IF EXISTS currency;
 
--
-Simple
-master
-table
+-- Simple master table
 CREATE TABLE currency (
   id     bigint unsigned NOT NULL AUTO_INCREMENT,
   name   VARCHAR(64) NOT NULL,
---
+++ gemini-app/src/main/resources/db/migration/V2.01.23112018__insert_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO currency
+VALUES (10001, 'Bitcoin', 'BTC', 0);
+INSERT INTO currency
+VALUES (10002, 'Ethereum', 'ETH', 0);

53e126fb8ac6643b0ff8fb289a8b0b15dd7d0384
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-23 21:39:52 +0700

    #2 - Add gemini-app module

---
+++ gemini-app/src/main/java/com/hendisantika/vuejs/geminiapp/GeminiAppApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.vuejs.geminiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeminiAppApplication.class, args);
+    }
+}
---
+++ gemini-app/src/test/java/com/hendisantika/vuejs/geminiapp/GeminiAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.vuejs.geminiapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeminiAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

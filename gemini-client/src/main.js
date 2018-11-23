d1c17984a30e51a1a902e9b300bb8b763725e1d4
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-24 05:18:01 +0700

    #19 - Modify main.js file - gemini-client

--- gemini-client/src/main.js
+++ gemini-client/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.config.productionTip = false;
+Vue.use(ElementUI, {locale});
 
 new Vue({
+    router,
+    store,
     render: h => h(App),
 }).$mount('#app');

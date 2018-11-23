4c68c466a17392ee05b5e5927781adc6b3cefcee
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-24 06:36:13 +0700

    #34 - Set firebase config - gemini-client

--- gemini-client/src/main.js
+++ gemini-client/src/main.js
@@ -5,8 +5,20 @@ import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import firebase from 'firebase'
+
 Vue.config.productionTip = false;
 Vue.use(ElementUI, {locale});
+
+var config = {
+    apiKey: "************************",
+    authDomain: "************** Firebaseapp.Com ********** ".,
+    DatabaseURL: ". ************************ Firebaseio.Com ",
+    ProjectId: "************************",
+    storageBucket: "******************* *****. appspot.com ",
+    messagingSenderId: " ************************ "
+};
+firebase.initializeApp(config);
 
 new Vue({
     router,

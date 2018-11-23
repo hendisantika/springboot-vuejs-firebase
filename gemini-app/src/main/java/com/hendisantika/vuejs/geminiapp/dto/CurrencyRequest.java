e639609d2fcf41599eaa616d185c6d28577698d7
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-24 04:53:55 +0700

    #12 - Add Currency Request - gemini-app module

---
+++ gemini-app/src/main/java/com/hendisantika/vuejs/geminiapp/dto/CurrencyRequest.java
@@ -0,0 +1,20 @@
+package com.hendisantika.vuejs.geminiapp.dto;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : gemini-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2018-11-24
+ * Time: 04:52
+ */
+@Data
+public class CurrencyRequest {
+
+    private String name;
+
+    private String symbol;
+}

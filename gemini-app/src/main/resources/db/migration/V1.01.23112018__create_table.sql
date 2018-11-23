d47b8cfd7e2152d9b13e3ae8d889fd0f14d4b279
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-23 21:49:15 +0700

    #5 - Add flyway migration script Database - gemini-app module

---
+++ gemini-app/src/main/resources/db/migration/V1.01.23112018__create_table.sql
@@ -0,0 +1,26 @@
+-
+Check
+existence
+so
+that
+tables
+do
+not
+overlap,
+delete
+if
+there
+are
+DROP TABLE IF EXISTS currency;
+
+-
+Simple
+master
+table
+CREATE TABLE currency (
+  id     bigint unsigned NOT NULL AUTO_INCREMENT,
+  name   VARCHAR(64) NOT NULL,
+  symbol VARCHAR(10) NOT NULL,
+  amount DECIMAL(40, 20),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

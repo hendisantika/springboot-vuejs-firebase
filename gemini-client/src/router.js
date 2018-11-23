d41bccb12ca1e6f6b4e00a09a9341cc8cf5c0fc0
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-24 06:49:30 +0700

    #38 - Add Navigation guard - gemini-client

--- gemini-client/src/router.js
+++ gemini-client/src/router.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import firebase from 'firebase'
 
 Vue.Use(Router);
 
-export default new Router({
+let router = new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
         {
             path: '/',
             name: 'home',
-            component: Home
+            component: Home,
+            meta: {RequiresAuth: true}
         },
         {
             path: '/about',
@@ -19,7 +21,8 @@ export default new Router({
             // route level code-splitting
             // this generate a separate chunk (about. [hash] .js) for this route
             // Which is Lazy //-Loaded When The Route Is Visited.
-            component: () => import(/* WebpackChunkName: "About" */ './views/About.Vue')
+            component: () => import(/* WebpackChunkName: "About" */ './views/About.Vue'),
+            meta: {requiresAuth: true}
         },
         {
             path: '/currency',
@@ -37,4 +40,24 @@ export default new Router({
             component: () => import (/* webpackChunkName: "singin" */' ./views/Signin.vue ')
         }
     ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+    if (requiresAuth) {
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                next()
+            } else {
+                next({
+                    path: '/signin',
+                    query: {redirect: to.fullPath}
+                })
+            }
+        })
+    } else {
+        next()
+    }
+});
+
+export default router

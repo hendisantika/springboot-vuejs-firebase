6135543bceb6b46b1ef18e0b4171f4a183bb08ed
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-24 05:24:45 +0700

    #21 - Add Currency.vue file - gemini-client

---
+++ gemini-client/src/components/Currency.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-row>
+        <el-col : Span="24">
+            <el-card Class="Box-Card">
+                <div Class="Clearfix" Slot="Header">
+                    <span> virtual currency List </span>
+                </div>
+                <div> ...</div>
+            </el-card>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+    export default {
+        name: "Currency"
+    }
+</script>
+
+<style scoped>
+
+</style>

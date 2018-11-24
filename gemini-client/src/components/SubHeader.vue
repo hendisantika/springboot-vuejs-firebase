e6f5110db35db889311916173d88779bf704adec
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-24 08:00:27 +0700

    #39 - Add Sub Header Component - gemini-client

---
+++ gemini-client/src/components/SubHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-button @Click="signout" class="signout-Label" type="text"> Signout</el-button>
+</template>
+
+<script>
+    import firebase from 'firebase'
+
+    export default {
+        name: "SubHeader",
+        methods: {
+            signout: function () {
+                firebase.Auth().signOut().then(() => {
+                    this.$router.push('/ Signin')
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

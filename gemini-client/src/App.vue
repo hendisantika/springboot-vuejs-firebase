33f9184a7d8d1816b01e9f674c904ef9d70d861a
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-24 08:03:19 +0700

    #41 - Modify App.vue file - gemini-client

--- gemini-client/src/App.vue
+++ gemini-client/src/App.vue
@@ -1,28 +1,43 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
+    <el-container id="app">
+        <el-header id="nav">
+            <global-header/>
+        </el-header>
+        <el-main>
+            <router-view/>
+        </el-main>
+    </el-container>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
+    import GlobalHeader from './components/GlobalHeader'
 
     export default {
-        name: 'app',
         components: {
-            HelloWorld
+            GlobalHeader
         }
     }
 </script>
 
-<style>
+<style lang="scss">
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
+    }
+
+    #nav {
+        padding: 10px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
     }
 </style>

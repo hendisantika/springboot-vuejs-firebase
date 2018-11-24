5f96ecbd6dab076148ac5f80c15922a241e67fb0
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-24 08:02:34 +0700

    #40 - Add Global Header Component - gemini-client

---
+++ gemini-client/src/components/GlobalHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="global-header">
+        <span class="global-header-label">Welcome to Gemini Application!</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GlobalHeader"
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "../styles/colors";
+
+    .global-header {
+        background-color: $HEADER_BACKGROUND;
+        height: 6.5vh;
+    }
+
+    .global-header-label {
+        color: $HEADER_LABEL;
+        font-weight: bold;
+        line-height: 6.5vh;
+    }
+</style>
